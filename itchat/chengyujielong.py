cc874a7fff42ba8adc68e71a0a262b0cfe1e6358
Alan <18655660717@163.com>
2018-05-10 17:02:13 +0800

    add passwd information python code

--- itchat/chengyujielong.py
+++ itchat/chengyujielong.py
@@ -3,9 +3,36 @@
 from urllib import parse, request
 from bs4 import BeautifulSoup
 import random
+from pypinyin import lazy_pinyin
+import Pinyin2Hanzi
+from Pinyin2Hanzi import DefaultDagParams, dag
+from itertools import chain
 
 
-def get_chengyu(end_str):
+def get_chengyu(word):
+	bigger_list = []
+	if Pinyin2Hanzi.is_chinese(word):
+		word_pinyin = lazy_pinyin(word)
+		dagParams = DefaultDagParams()
+		word_list = []
+		result = dag(dagParams, word_pinyin, path_num=3, log=True)
+		for item in result:
+			word_list.append(item.path[0])
+		# print(word_list)
+		for avg_word in word_list:
+			if find_chengyu(avg_word) != "Null":
+				bigger_list.append(find_chengyu(avg_word))
+
+		heiheihei = list((chain(*bigger_list)))
+		# print(heiheihei)
+		max_len = len(heiheihei) - 1
+		flag = random.randint(0, max_len)
+		return heiheihei[flag]
+	else:
+		return "找不到了，我发红包吧"
+
+
+def find_chengyu(end_str):
 	temp_list = []
 	end_str = end_str.encode('gb2312')
 	header = {'Useragent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
@@ -16,15 +43,22 @@ def get_chengyu(end_str):
 	post_args = parse.urlencode(post_args).encode('gb2312')
 	req = request.Request(base_url, headers=header, data=post_args)
 	html = request.urlopen(req).read()
-	soup = BeautifulSoup(html, 'html.parser', from_encoding="gb2312")
+	soup = BeautifulSoup(html, 'html.parser', from_encoding="gb18030")
 	big_list = soup.table.contents[1].contents[1].contents[1].contents[11].contents[2:]
 	[big_list.remove(i) for i in big_list if i == '\n']
 	for j in big_list:
 		temp_list.append(str(j).split('u', 1)[1].split('>')[1].split('<')[0].strip())
-	max_len = len(temp_list) - 1
-	flag = random.randint(0, max_len)
-	return temp_list[flag]
+	fin_list = temp_list[:]
+	for k in temp_list:
+		if k[0].encode('gb2312') != end_str:
+			fin_list.remove(k)
+	# print(fin_list)
+	if len(fin_list) == 0:
+		return "Null"
+	else:
+		return fin_list
 
 
 if __name__ == '__main__':
-	print(get_chengyu("二"))
+	# print(find_chengyu("哈"))
+	print(get_chengyu("淼"))
--- itchat/wechat.py
+++ itchat/wechat.py
@@ -4,9 +4,34 @@ import itchat
 from bs4 import BeautifulSoup
 from urllib import parse, request
 import random
+from pypinyin import lazy_pinyin
+import Pinyin2Hanzi
+from Pinyin2Hanzi import DefaultDagParams, dag
+from itertools import chain
 
 
-def get_chengyu(end_str):
+def get_chengyu(word):
+	bigger_list = []
+	if Pinyin2Hanzi.is_chinese(word):
+		word_pinyin = lazy_pinyin(word)
+		dagParams = DefaultDagParams()
+		word_list = []
+		result = dag(dagParams, word_pinyin, path_num=3, log=True)
+		for item in result:
+			word_list.append(item.path[0])
+
+		for avg_word in word_list:
+			bigger_list.append(find_chengyu(avg_word))
+
+		heiheihei = list((chain(*bigger_list)))
+		max_len = len(heiheihei) - 1
+		flag = random.randint(0, max_len)
+		return heiheihei[flag]
+	else:
+		return "Null"
+
+
+def find_chengyu(end_str):
 	temp_list = []
 	end_str = end_str.encode('gb2312')
 	header = {'Useragent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
@@ -17,14 +42,20 @@ def get_chengyu(end_str):
 	post_args = parse.urlencode(post_args).encode('gb2312')
 	req = request.Request(base_url, headers=header, data=post_args)
 	html = request.urlopen(req).read()
-	soup = BeautifulSoup(html, 'html.parser', from_encoding="gb2312")
+	soup = BeautifulSoup(html, 'html.parser', from_encoding="gb18030")
 	big_list = soup.table.contents[1].contents[1].contents[1].contents[11].contents[2:]
 	[big_list.remove(i) for i in big_list if i == '\n']
 	for j in big_list:
 		temp_list.append(str(j).split('u', 1)[1].split('>')[1].split('<')[0].strip())
-	max_len = len(temp_list) - 1
-	flag = random.randint(0, max_len)
-	return temp_list[flag]
+	fin_list = temp_list[:]
+	for k in temp_list:
+		if k[0].encode('gb2312') != end_str:
+			fin_list.remove(k)
+	# print(fin_list)
+	if len(fin_list) == 0:
+		return "Null"
+	else:
+		return fin_list
 
 
 # @itchat.msg_register(itchat.content.TEXT)
@@ -38,9 +69,14 @@ def group_reply(msg):
 	from_user = msg['ActualNickName']
 	group_id = msg['FromUserName']
 	print(group_name, from_user, group_id, msg['Text'])
+	print(msg['Text'][-1])
+	temp_str = msg['Text'][-1]
 
-	if group_name == '胡店长和他的伙计们' and from_user == "随性，":
-		itchat.send("test from group %s from user %s" % (group_name, from_user), toUserName=group_id)
+	if group_name == 'test' and from_user == '我不是我':
+		# itchat.send("test from group %s from user %s" % (group_name, from_user), toUserName=group_id)
+		send_msg = get_chengyu(temp_str)
+		if send_msg != "Null":
+			itchat.send(send_msg, toUserName=group_id)
 
 
 itchat.auto_login(True)
