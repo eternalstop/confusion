adea54b1d41c493915f78d065fa626e03f6e956d
eternalstop <18655660717@163.com>
2019-09-16 17:35:16 +0800

    add passwd information python code

--- itchat/temp.py
+++ itchat/temp.py
@@ -4,6 +4,8 @@ from pypinyin import pinyin, lazy_pinyin, Style
 import Pinyin2Hanzi
 from Pinyin2Hanzi import DefaultDagParams
 from Pinyin2Hanzi import dag
+from itertools import chain
+import random
 
 
 def pinyin_2_hanzi(word):
@@ -11,16 +13,16 @@ def pinyin_2_hanzi(word):
 		word_pinyin = lazy_pinyin(word)
 		dagParams = DefaultDagParams()
 		word_list = []
-		result = dag(dagParams, word_pinyin, path_num=3, log=True)
+		result = dag(dagParams, word_pinyin, path_num=4, log=True)
 		for item in result:
 			word_list.append(item.path[0])
-		return word_list
-	else:
-		return "Null"
+		# print(word_list)
+		for avg_word in word_list:
+			print(avg_word)
 
 
 if __name__ == '__main__':
-	print(pinyin_2_hanzi("666"))
+	print(pinyin_2_hanzi("花"))
 
 
 # print(pinyin('好'))
