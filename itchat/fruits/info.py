b1a9d5ff7922149544fa7ba9b4d1bb83c648b8f7
eternalstop <18655660717@163.com>
2019-09-18 15:25:13 +0800

    add passwd information python code

--- itchat/fruits/info.py
+++ itchat/fruits/info.py
@@ -2,6 +2,18 @@
 import requests
 
 
+def login(username, password):
+	token_link = "http://fruits.eternalstop.com:7788/api-token-auth/"
+	params = {
+		"username": username,
+		"password": password,
+	}
+	
+	reponse = requests.post(token_link, data=params)
+	token = reponse.json()["token"]
+	return token
+
+
 def get_fruit_list(token):
 	fruits_link = "http://fruits.eternalstop.com:7788/fruit/"
 	header = {
@@ -15,18 +27,38 @@ def get_fruit_list(token):
 		return results
 
 
-def login(username, password):
-	token_link = "http://fruits.eternalstop.com:7788/api-token-auth/"
-	params = {
-		"username": username,
-		"password": password,
+def get_product_id(name, products):
+	for product in products:
+		if product["name"] in name:
+			return product["id"]
+
+
+def post_order_info(token, info, products):
+	order_link = "http://fruits.eternalstop.com:7788/order/"
+	header = {
+		"Authorization": 'JWT ' + token
 	}
-	
-	reponse = requests.post(token_link, data=params)
-	token = reponse.json()["token"]
-	return token
+	results_data = {}
+	for i in info:
+		post_data = {
+			"deliverytime": '',
+			"lastcheck": i["上次查询时间"],
+			"ordernum": i["单号"],
+			"company": "",
+			"checkurl": i["链接"],
+			"name": i["收货人"],
+			"phonenum": i["电话"],
+			"location": i["地址"],
+			"product": get_product_id(name=i["产品"], products=products)
+		}
+		response = requests.post(url=order_link, headers=header, data=post_data)
+		if response.status_code == 200:
+			continue
+		else:
+			results_data[i["单号"]] = "Faild"
+	return results_data
 
 
 if __name__ == '__main__':
 	getoken = login(username="admin", password="Missy@u777")
-	print(get_fruit_list(getoken))
+	products = get_fruit_list(getoken)
